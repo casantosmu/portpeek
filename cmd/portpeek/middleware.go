a0fe0424f085be6c05617a995a61a47b156ec13c
Carlos Santos <casantosmu@gmail.com>
2026-08-06 11:58:34 +0200

    Skip health check logs

--- cmd/portpeek/middleware.go
+++ cmd/portpeek/middleware.go
@@ -26,6 +26,11 @@ func authMiddleware(apiKey string) middleware {
 func logMiddleware(realIPHeader string) middleware {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if r.URL.Path == "/health" {
+				next.ServeHTTP(w, r)
+				return
+			}
+
 			startedAt := time.Now()
 			next.ServeHTTP(w, r)
 			log.Printf(
